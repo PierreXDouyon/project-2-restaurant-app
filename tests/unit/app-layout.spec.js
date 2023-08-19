f62787315d5dfba794a46ee4bec9d57272f02437
Pierre Douyon <pierredouyon514@gmail.com>
2023-08-19 15:54:42 -0400

    Added a layout

---
+++ tests/unit/app-layout.spec.js
@@ -0,0 +1,106 @@
+Header.vue
+
++-------------------------------+
+| Logo & Title                  |
++-------------------------------+
+| Search Input                  |
++-------------------------------+
+| Hamburger Menu                |
++-------------------------------+
+
+
+HomeView.vue
+
++-------------------------------+
+| Search Input & Button         |
++-------------------------------+
+| Filter Buttons & View         |
+| Restaurant Cards              |
+| View More Button              |
++-------------------------------+
+
+    
+Footer.vue
+
++-------------------------------+
+| Copyright Information         |
++-------------------------------+
+| Social Icons                  |
++-------------------------------+
+
+MyWatchListView.vue
+
++-------------------------------+
+| Search by List & Cuisine      |
+| Search & Clear Button         |
++-------------------------------+
+| Watchlist Restaurants         |
+| Edit & Delete Buttons         |
+| Move Restaurant Button        |
++-------------------------------+
+
+    
+ReservationView.vue
+
++-------------------------------+
+| Restaurant Detail for         |
+| Reservation                   |
++-------------------------------+
+| Dine-in & Seat Options        |
+| Reserve Now Button            |
++-------------------------------+
+
+
+RestaurantDetail.vue
+
++-------------------------------+
+| Restaurant Name               |
++-------------------------------+
+| Restaurant Image              |
++-------------------------------+
+| Cuisine Name                  |
++-------------------------------+
+| View More & Add to Watchlist  |
+| Buy Reservations & Delivery   |
+    +------------------------------- +
+    
+
+RestaurantInfoView.vue
+
++-------------------------------+
+| Restaurant Details            |
++-------------------------------+
+| Opened In & Address           |
++-------------------------------+
+| Phone, Website & Rating       |
++-------------------------------+
+| Menu Items & Availability     |
++-------------------------------+
+| Add to Watchlist & Delivery   |
++-------------------------------+
+
+    
+RestaurantList.vue
+
++-------------------------------+
+| List of Restaurant Cards      |
++-------------------------------+
+
+    
+ScrollToTop.vue
+
++-------------------------------+
+| Scroll to Top Button          |
++-------------------------------+
+
+    
+DeliveryNowView.vue
+
++-------------------------------+
+| Search by Name & Genre        |
+| Search & Clear Button         |
++-------------------------------+
+| Delivery Restaurant Cards     |
+| Add to Watchlist & Delivery   |
+| Icons & Delivery Now Button   |
++-------------------------------+
