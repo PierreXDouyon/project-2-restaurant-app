1a60bea9240d395db9d66dee96a581fff76e3dd4
PierreXDouyon <118191281+PierreXDouyon@users.noreply.github.com>
2023-08-20 09:05:45 -0400

    Update restaurant-details.spec.js

--- tests/unit/restaurant-details.spec.js
+++ tests/unit/restaurant-details.spec.js
@@ -5,11 +5,15 @@ describe("RestaurantDetail.vue", () => {
 
   it("displays cuisine name", () => {});
 
+  it("displays Opened in: Date", () => {});
+
+  it("displays four menu items", () => {});
+
   it('emits "show-more" event when "View More" button is clicked', async () => {});
 
   it('emits "add-to-watchlist" event when "Add To Watchlist" button is clicked', async () => {});
 
-  it('emits "reservations" event when "Buy reservations" button is clicked', async () => {});
+  it('emits "reservations" event when "Book reservations" button is clicked', async () => {});
 
   it('emits "Delivery-now" event when "Delivery Now" button is clicked', async () => {});
 });
